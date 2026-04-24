13181eca5376878883bfcb02179f44bc36c09ca6
Alexander Lipov <lipov.sa@mail.ru>
2026-04-24 02:19:25 -0700

    feat: add refresh and users

--- app/api/v1/api.py
+++ app/api/v1/api.py
@@ -1,9 +1,10 @@
 from fastapi import APIRouter
 
-from app.api.v1.endpoints import auth, chain, projects
+from app.api.v1.endpoints import auth, chain, projects, users
 
 api_router = APIRouter()
 
 api_router.include_router(chain.router, prefix="/chain", tags=["Chain"])
 api_router.include_router(projects.router, prefix="/projects", tags=["Projects"])
 api_router.include_router(auth.router, prefix="/auth", tags=["Auth"])
+api_router.include_router(users.router, prefix="/users", tags=["Users"])
